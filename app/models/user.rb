491478b8523ff5639e6b96faa26b41454cb78ad8
tolaseadegbite <tolasekelvin@gmail.com>
2023-05-27 10:30:20 +0100

    Write basic user test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,19 @@
 class User < ApplicationRecord
+  before_save { downcase_username }
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :username, uniqueness: true, allow_blank: true
+
+
+
+  private
+
+  # Converts username to all lowercase.
+  def downcase_username
+    self.username = username.downcase
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,11 +3,26 @@ require "test_helper"
 class UserTest < ActiveSupport::TestCase
   
   def setup
-    @user = User.new(email: "lashe@foobar.com", password: "foobar", password_confirmation: "foobar")
+    @user = User.new(username: "", email: "lashe@foobar.com", password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
     assert @user.valid?
+  end
+
+  test "usernames should be lowcase" do
+    mixed_case_username = "tOlASe"
+    @user.username = mixed_case_username
+    @user.save
+    assert_equal mixed_case_username.downcase, @user.reload.username
+  end
+
+  test "usernames should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.username = "tolase"
+    @user.username = "tolase"
+    @user.save
+    assert_not duplicate_user.valid?
   end
 
 end
