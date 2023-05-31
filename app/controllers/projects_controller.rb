79020d9fb2cd5afda328fdc77df4c05b970f461e
tolaseadegbite <tolasekelvin@gmail.com>
2023-05-31 23:47:33 +0100

    Add pagy

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,6 +5,11 @@ class ProjectsController < ApplicationController
 
     def index
         @projects = Project.includes(:user, image_attachment: :blob).order(created_at: :desc)
+        @pagy, @projects = pagy_countless(@projects, items: 6)
+        respond_to do |format|
+            format.html
+            format.turbo_stream
+        end
     end
 
     def new
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
+    include Pagy::Frontend
 
     def full_title(page_title="")
         base_title = "Tolase Kelvin Adegbite"
