453f9fa570f0aa404c89007ed491d0a650b3f3be
tolaseadegbite <tolasekelvin@gmail.com>
2023-06-07 08:57:33 +0100

    Add likes view

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,7 +4,7 @@ class LikesController < ApplicationController
     def create
         @like = current_user.likes.new(like_params)
         if @like.save
-            redirect_to :back
+            redirect_back(fallback_location: root_path)
         else
             flash[:notice] = @like.errors.full_messages.to_sentence
         end
@@ -14,7 +14,7 @@ class LikesController < ApplicationController
         @like = current_user.likes.find(params[:id])
         likeable = @like.likeable
         @like.destroy
-        redirect_to :back
+        redirect_back(fallback_location: root_path)
     end
 
     private
