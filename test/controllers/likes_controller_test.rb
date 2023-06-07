ae1b5d7d34e1b6d5146d602fe47bb7832b3f4dde
tolaseadegbite <tolasekelvin@gmail.com>
2023-06-07 21:33:34 +0100

    Test likes controller and model

--- test/controllers/likes_controller_test.rb
+++ test/controllers/likes_controller_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class LikesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user =     users(:tolase)
+    @project =  projects(:orange)
+  end
+
+  test "should redirect when not logged in" do
+    assert_no_difference 'Like.count' do
+      post likes_path, params: { like: { likeable: @project, likeable_type: "Project" } }
+    end
+    assert_not flash.empty?
+  end
 end
--- test/models/like_test.rb
+++ test/models/like_test.rb
@@ -1,7 +1,30 @@
 require "test_helper"
 
 class LikeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
+  def setup
+    @user = users(:tolase)
+    @project = projects(:orange)
+    @like = @user.likes.build(likeable: @project, likeable_type: 'Project')
+    sign_in @user
+  end
+
+  test "should be valid" do
+    assert @like.valid?
+  end
+
+  test "user_id should be present" do
+    @like.user_id = nil
+    assert_not @like.valid?
+  end
+
+  test "likeable should be present" do
+    @like.likeable = nil
+    assert_not @like.valid?
+  end
+
+  # test "uniqueness of user_id on likeable" do
+  #   dup_like_user = @like.user.dup
+  #   @like.save
+  #   assert_not dup_like_user.valid?
   # end
 end
